55e70093ad34820dc4e470911f7f5257a65001f0
AbhinavT2 <abhinav.tumu1@gmail.com>
2019-09-26 13:10:07 -0700

    Updates

---
+++ precache-manifest.161c572a611963de8d66e8a25ff59d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5959562259f5b7b653aab83aca5d2447",
+    "url": "/tumu.dev/index.html"
+  },
+  {
+    "revision": "0a6e2e6c589018a6c243",
+    "url": "/tumu.dev/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c05e47e898078b2a9bd3",
+    "url": "/tumu.dev/static/js/2.4b6d1332.chunk.js"
+  },
+  {
+    "revision": "0a6e2e6c589018a6c243",
+    "url": "/tumu.dev/static/js/main.7834ec25.chunk.js"
+  },
+  {
+    "revision": "8662089f5b046bf16abd",
+    "url": "/tumu.dev/static/js/runtime-main.fcaa77e4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tumu.dev/static/media/logo.25bf045c.svg"
+  }
+]);
