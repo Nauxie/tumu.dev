7fbd7b0bafb017964d8d9a3a58a747168574c013
AbhinavT2 <abhinav.tumu1@gmail.com>
2019-09-26 17:24:10 -0700

    profpic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import pic from './images/abhinav.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pic} className="App-logo" alt="logo" />
         <p>
           SUP TEEKERS
         </p>
