40e6231ed7e0b0c9179afc4c6ac8840bdf0ff3a8
AbhinavT2 <abhinav.tumu1@gmail.com>
2019-09-27 11:33:39 -0700

    design elements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,36 +4,50 @@ import pic from './images/abhinav3.png'
 import './App.css';
 import Typing from 'react-typing-animation';
 
+const name = "{Abhinav Tumu}"
+
 function App() {
   return (
     <div className="Background">
-      <Typing>
-        <h2>Abhinav Tumu</h2>
+      <Typing speed={100}>
+        <h1>{name}</h1>
+
       </Typing>
       <img className="profpic" src={pic} alt="logo" />
 
+
+      <p className="text1">
+        Work in progress! Check back later :)
+      </p>
       <div className="links">
         <a
           href="https://www.linkedin.com/in/abhinav-tumu-863635170/"
           target="_blank"
           rel="noopener noreferrer"
+          className="link"
         >
           LinkedIn
         </a>
-        <p>
-        </p>
+
         <a
           href="https://github.com/Nauxie"
           target="_blank"
           rel="noopener noreferrer"
+          className="link"
         >
           Github
         </a>
+        <a
+          href="https://devpost.com/Nauxie?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link"
+        >
+          Devpost
+        </a>
 
       </div>
-      <p>
-        Work in progress! Check back later :)
-        </p>
+
     </div>
   );
 }
