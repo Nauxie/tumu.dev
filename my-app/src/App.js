b06e5dbb1f541b10077caa00c8f4e0053adbb8e7
AbhinavT2 <abhinav.tumu1@gmail.com>
2019-09-30 13:21:51 -0700

    webedits

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import pic from './images/abhinav3.png'
 import './App.css';
 import Typing from 'react-typing-animation'
@@ -8,14 +7,17 @@ import red from './images/redux.png'
 import nod from './images/node.png'
 import ker from './images/keras.jpg'
 import tensor from './images/tensorflow.png'
-import images from './images.js'
+import Particles from 'react-particles-js'
 
 const name = "{Abhinav Tumu}"
 
 
 function App() {
   return (
+    
     <div className="Background">
+      
+
       <Typing speed={100}>
         <h1>{name}</h1>
 
@@ -52,7 +54,7 @@ function App() {
         >
           <img className="smallpic" src='https://seeklogo.com/images/D/devpost-logo-95FF685C5D-seeklogo.com.png' alt='Devpost' />
         </a>
-        
+
         <a
           href="https://drive.google.com/file/d/1VYiJAIV1bRPoLm8FTjmvuRKIvYH0sgWT/view?usp=sharing"
           target="_blank"
@@ -67,19 +69,19 @@ function App() {
       <div>
         <div>
           <p className='titletype'>Languages</p>
-          <a href="https://reactjs.org/" title="Javascript">
+          <a title="Javascript">
             <img className="icon2" src='http://pluspng.com/img-png/javascript-vector-png-javascript-vector-logo-600.png' alt="logo" />
           </a>
-          <a href="https://reactjs.org/" title="Python">
+          <a title="Python">
             <img className="icon2" src='http://www.iconarchive.com/download/i73027/cornmanthe3rd/plex/Other-python.ico' alt="logo" />
           </a>
-          <a href="https://reactjs.org/" title="Java">
+          <a title="Java">
             <img className="icon2" src='https://www.stickpng.com/assets/images/58480979cef1014c0b5e4901.png' alt="logo" />
           </a>
-          <a href="https://reactjs.org/" title="HTML5">
+          <a title="HTML5">
             <img className="icon2" src='https://cdn.freebiesupply.com/logos/large/2x/html-5-logo-png-transparent.png' alt="logo" />
           </a>
-          <a href="https://reactjs.org/" title="CSS">
+          <a title="CSS">
             <img className="icon2" src='https://redpixel.com/_uploads/CSS3_Badge_512.png?w=640' alt="logo" />
           </a>
 
@@ -105,6 +107,11 @@ function App() {
         </div>
 
       </div>
+      <div className="bottomtext">
+        Powered by React and Github Pages
+
+      </div>
+      
 
     </div>
   );
